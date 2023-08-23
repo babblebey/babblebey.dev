6500939a50b4b477ce3501ca38dc9e683460c9af
babblebey <babblebey@gmail.com>
2023-08-23 03:14:59 +0100

    feat: implement dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
-import "globals.css"
+import "@/styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { siteConfig } from "@/config/site";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -58,7 +59,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} dark`}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
