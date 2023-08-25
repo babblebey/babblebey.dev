ffd41b85089008d7d73415a6d1e692d89ff7e385
babblebey <babblebey@gmail.com>
2023-08-25 19:20:26 +0100

    chore: modify layout `main` area

--- app/(www)/layout.tsx
+++ app/(www)/layout.tsx
@@ -1,5 +1,5 @@
-import { MainNav } from "@/components/main-nav"
-import { navConfig } from "@/config/nav"
+import { MainNav } from "@/components/main-nav";
+import { navConfig } from "@/config/nav";
 
 interface IndexPageLayoutProps {
   children: React.ReactNode
@@ -9,7 +9,9 @@ export default async function IndexPageLayout({ children }: IndexPageLayoutProps
   return (
     <div className="flex min-h-screen flex-col">
       <MainNav items={navConfig.items} />
-      {children}
+      <main className="container max-w-6xl px-4 sm:px-8">
+        {children}
+      </main>
     </div>
   )
 }
